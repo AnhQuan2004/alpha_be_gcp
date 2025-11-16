b75dac2cf5c1d2a30ec9e8c16cb6d096c3262440
AnhQuan2004 <anhquan200304@gmail.com>
2025-11-16 20:51:12 +0700

    fix code

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker build -t data_binance_alpha .
-docker tag data_binance_alpha_gcp jasong03/data_binance_alpha:latest
+docker tag data_binance_alpha jasong03/data_binance_alpha:latest
 docker push jasong03/data_binance_alpha:latest
